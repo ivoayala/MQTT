e774cd145bf13f1cacf0bb383589b33896379c83
Ivo A <ivoayala@hotmail.com>
2017-07-07 00:03:51 -0500

    Update 170706_publisher.py

--- 170706_publisher.py
+++ 170706_publisher.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import paho.mqtt.client as mqtt
-
+import time #import time module
 # This is the Publisher
 
 client = mqtt.Client()
 client.connect("localhost",1883,60)
-client.publish("topic/test", "Hello world!");
+while client.loop()==0:
+	client.publish("topic/test", "Hello world!");
+	time.sleep(1) #sleep for 10 secs before next call
+
 client.disconnect();
-
-
